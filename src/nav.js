7eb85e15f0aa74b80d397e8f35888cb8dd378068
Julian Ponce <juliangponce@gmail.com>
2021-08-16 16:01:29 -0300

    se agrego estilos

--- src/nav.js
+++ src/nav.js
@@ -4,9 +4,9 @@ const nav =()=>{
         <header>
            <h1>e-commerce</h1>
            <nav class="navbar navbar-dark bg-dark">
-                <form class="container-fluid justify-content-start">
-                        <button class="btn btn-outline-success me-2" type="button">1</button>
-                        <button class="btn btn-sm btn-outline-secondary" type="button">2</button>
+                <form class="container-fluid justify-content-center">
+                        <button class="btn btn-outline-success me-2" type="button">categoraias</button>
+                        <button class="btn btn-outline-success me-2" type="button">contacto</button>
                 </form>
            </nav>
            
