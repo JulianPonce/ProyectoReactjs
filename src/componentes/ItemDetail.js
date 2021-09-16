a7974c1747466fd3402c0e7852738407a5b55ec9
Julian Ponce <juliangponce@gmail.com>
2021-09-16 16:03:28 -0300

    se agrego funciones en context

--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -6,18 +6,22 @@ import { CartContext } from "../contexto/CartContext";
 const ItemDetail = ({element}) => {
     
     const [finish, setFinish] = useState(false)
-    const {addItem} = useContext(CartContext)
+    const {addItem,clear,cart,removeItem} = useContext(CartContext)
 
     const onAdd = (contador) => {
         alert("se agregaron"  + contador  + "productos")
         addItem(element, contador)
     }
 
+ 
+   
+   
     return(
         
         <>
         <li key={element.id}>{element.categoria} {element.precio}$</li>
         {finish === true ? <button > Ir al carrito </button> : <ItemCount  stock={4} initial={1} onAdd={onAdd} setFinish={setFinish} />}
+        <button onClick={clear}> Borrar items </button> 
         </>
     
         )
--- src/contexto/CartContext.js
+++ src/contexto/CartContext.js
@@ -9,15 +9,48 @@ export const CartContextProvider = ({children}) => {
     const [cart,setCart]= useState([])
    
     const addItem = (item , quantity) => {
+        
+        if(isInCart(item.id)){
+  
+         const upDateCart = [...cart];
+           
+            upDateCart.forEach((element)=>{
+               if(element.item.id === item.id){
+                   element.quantity += quantity
+               }
+
+            })
+
+
+        }else{
         setCart([...cart,{item,quantity}])
-    }
-    console.log(cart);
+        }
     
+    }
+     console.log(cart);
+    
+    
+     const isInCart = (id) => {
+       
+        return cart.find(element =>element.item.id === id)
+    
+    }
+    
+    const clear = () => setCart([])
+
+
+    const removeItem = ( itemId) => {
+
+        const cartFilter = cart.filter(element => element.item.id !== itemId)
+        return setCart(cartFilter)
+    }
     
     return(
     
-       <CartContext.Provider value ={{addItem}}>
+       <CartContext.Provider value ={{addItem,clear, removeItem,cart}}>
            {children}
         </CartContext.Provider >  
     )
+
+
 }
